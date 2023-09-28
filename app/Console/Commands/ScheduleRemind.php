362355eaad704ddcd8282bd6551fe2353b69c9c5
datdt <thanhdat02112001pt@gmail.com>
2023-09-28 11:52:34 +0700

    done remind and push noti

--- app/Console/Commands/ScheduleRemind.php
+++ app/Console/Commands/ScheduleRemind.php
@@ -4,39 +4,38 @@ namespace App\Console\Commands;
 
 use App\Models\Todo;
 use App\Models\User;
-use Carbon\Carbon;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Auth;
 
-class NotifyDueTasks extends Command
+class ScheduleRemind extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'app:notify-due-tasks';
+    protected $signature = 'app:schedule-remind';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Notify task about to due';
+    protected $description = 'Schedul to push notification at setted remind time';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        $about_due_tasks = Todo::where('status', '=', 0)->whereBetween('due_date', [now(), now()->addHour()])->get();
-        foreach ($about_due_tasks as $task) {
-            $userDeviceToken =  User::find($task->user_id)->device_token;
+        $tasks_need_reminds = Todo::where('status', '=', 0)->whereBetween('remind', [now()->startOfMinute(), now()->endOfMinute()])->get();
+        foreach($tasks_need_reminds as $task)
+        {
+            $userDeviceToken = User::find($task->user_id)->device_token;
             $data = [
                 "registration_ids" => array($userDeviceToken),
                 "notification" => [
-                    "title" => $task->name . " is about to due",
-                    "body" => $task->name. " is going to due at ". $task->due_date,  
+                    "title" => "Remind from TODO",
+                    "body" => "You have to do " . $task->name . ". Please check it out!",  
                 ]
             ];
             $dataString = json_encode($data);
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('app:notify-due-tasks')->hourly();
+        $schedule->command('app:schedule-remind')->everyMinute()->withoutOverlapping();
     }
 
     /**
